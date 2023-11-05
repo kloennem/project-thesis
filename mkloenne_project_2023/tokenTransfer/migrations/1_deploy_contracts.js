7709f2adbde0da30bb1611ca0726f58d7f46f966
kloennem <marie.kloenne@tuhh.de>
2023-11-05 13:44:01 +0100

    write deploy_contracts

--- mkloenne_project_2023/tokenTransfer/migrations/1_deploy_contracts.js
+++ mkloenne_project_2023/tokenTransfer/migrations/1_deploy_contracts.js
@@ -1,5 +1,13 @@
 // const todo: add contract name = artifacts.require("todo: add contract file name");
 
-// module.exports = function(deployer) {
+const MetaForwarder = artifacts.require("MetaForwarder");
+const TokenContract = artifacts.require("Protocol2");
+const TxInclusionVerifier = artifacts.require("MockedTxInclusionVerifier");
+
+module.exports = async function(deployer, network) {
 //  deployer.deploy(todo: add contract name);
-// };
+    await deployer.deploy(MetaForwarder);
+    await deployer.deploy(TxInclusionVerifier, 0, 0, true);
+    await deployer.deploy(TokenContract, [], TxInclusionVerifier.address, 100, MetaForwarder.address);
+    await deployer.deploy(TokenContract, [], TxInclusionVerifier.address, 100, MetaForwarder.address);
+};
